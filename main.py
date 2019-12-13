08d9106125076d50b345927fdec1114b67e2bbd5
asariakevin <asariakevin@gmail.com>
2019-12-13 11:11:45 +0300

    add objects and functionality to the bedroom

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from state_machine.state_machine import StateMachine
 import utils
 
 regular_user_prompt = "$ "
-super_user_prompt = "#"
+super_user_prompt = "# "
 state_machine = StateMachine()
 
 print(utils.beginning_message)
@@ -15,6 +15,24 @@ def user_choice(choice):
     if choice == "cd door_to_house":
         
         state_machine.enter_house()
+
+    elif choice == "cd door_to_outside_house":
+        
+        state_machine.leave_house()
+    elif choice == "cd door_to_bedroom":
+        
+        state_machine.enter_bedroom()
+
+    elif choice == "cd door_to_leave_bedroom":
+        
+        state_machine.leave_bedroom()
+
+    elif choice == "cd door_to_studyroom":
+        
+        state_machine.enter_studyroom()
+    elif choice == "cd door_to_leave_studyroom":
+        
+        state_machine.leave_studyroom()
 
     elif choice == "ls":
         
--- state_machine/rooms/bedroom.py
+++ state_machine/rooms/bedroom.py
@@ -3,27 +3,31 @@ from .room_interface import Room
 class Bedroom(Room):
     def __init__(self, context):
         self.context = context
+        self.collection_of_objects = ["bed","table","chair","door_to_leave_bedroom"]
+        self.hidden_objects = ["journal"]
+        self.files = {"journal" : "journal.txt"}
 
     def enter_house(self):
-        print("you are in the house")
+        print("You are already in the house")
 
     def leave_house(self):
-        print("you are now outside the house")
+        print("You must first head into the livingroom in order to leave the house")
 
     def enter_bedroom(self):
         print("you are now inside the bedroom")
         
 
     def leave_bedroom(self):
-        print("you are outside the house")
+        self.context.state = self.context.in_house
+        print("you are now in the livingroom")
         
 
     def enter_studyroom(self):
-        print("you are now inside the studyroom")
+        print("You must first head into the livingroom in order to enter the studyroom")
         
 
     def leave_studyroom(self):
-        print("you are outside the house")
+        print("You can't as you are in the bedroom")
         
 
 
